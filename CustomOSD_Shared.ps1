af5c05d7b31eedbe661f237a476fa0047f075ca7
CCC-jmeddaugh <jmeddaugh@cornerstonemarshfield.com>
2025-10-16 07:53:48 -0500

    Create CustomOSD_Shared.ps1

---
+++ CustomOSD_Shared.ps1
@@ -0,0 +1,10 @@
+# Shared devices naming: WIN-<Serial>
+try {
+    $serial = (Get-CimInstance Win32_BIOS).SerialNumber -replace '\s',''
+    $newName = "WIN-$serial".Substring(0, [math]::Min(15, "WIN-$serial".Length))
+    Write-Host "Setting computer name to $newName"
+    reg add "HKLM\System\Setup" /v "ComputerName" /t REG_SZ /d $newName /f | Out-Null
+}
+catch {
+    Write-Warning "Failed to set computer name"
+}
